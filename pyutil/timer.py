6524ce3683b9c8c72a725bace206452a3769d958
Chongdan Pan <panchongdan@foxmail.com>
2024-01-05 10:34:05 -0500

    add comments to timer

--- pyutil/timer.py
+++ pyutil/timer.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 from typing import Callable, Dict, List
 
+
 class Wrapped(object):
     def __init__(self, func: Callable, info: Dict[str, List[float]]):
         name = func.__name__
@@ -9,20 +10,35 @@ class Wrapped(object):
             info[name] = list()
         self.records = info[name]
         self.func = func
+        
     def __call__(self, *args, **kwargs):
         t = time.time()
         res = self.func(*args, **kwargs)
         self.records.append(time.time() - t)
         return res
+    
             
 class Timer(object):
+    '''A decorator class to profiling the time usage of functions
+    
+    Examples:
+    # Decorate a function
+    >>> @Timer.wrap
+    >>> def my_func():
+    >>>     pass
+    
+    # Decorate a method
+    >>> cls.func = Timer.wrap(cls)
+
+    '''
     info = dict()  
+
     @classmethod
-    def wrap(cls, func):
+    def wrap(cls, func: Callable) -> Wrapped:
         return func if isinstance(func, Wrapped) else Wrapped(func, cls.info)
     
     @classmethod
-    def get_stats(cls):
+    def get_stats(cls) -> pd.DataFrame:
         stats = list()
         for func, rel in cls.info.items():
             rel = pd.Series(rel)
@@ -32,4 +48,3 @@ class Timer(object):
     @classmethod
     def reset(cls):
         cls.info.clear()
-        
